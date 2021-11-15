c10d71d00d41a23dcc1a26cafdba0015c684c6b7
your_github_username <your_github_email>
2021-11-15 09:30:48 +0700

    initial commit with swagger config

---
+++ src/main/java/com/auth/koperasi/koperasiauth/KoperasiAuthApplication.java
@@ -0,0 +1,13 @@
+package com.auth.koperasi.koperasiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoperasiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoperasiAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/koperasi/koperasiauth/KoperasiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.koperasi.koperasiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoperasiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
